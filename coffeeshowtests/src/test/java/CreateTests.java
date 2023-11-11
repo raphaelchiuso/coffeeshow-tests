e8d0fa771a6efb7df41ed4fe004724b3ea5884c0
raphaelchiuso <raphael.chiuso@gmail.com>
2023-11-11 13:30:13 -0300

    test: open add item modal.

--- coffeeshowtests/src/test/java/CreateTests.java
+++ coffeeshowtests/src/test/java/CreateTests.java
@@ -1,14 +1,19 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CreateTests {
     private final String addItemButtonId = "create";
+    private final String addItemModalId = "chakra-modal-:R7b5sm:";
     private final TestHelper testHelper = new TestHelper();
 
     @BeforeEach
@@ -19,5 +24,12 @@ public class CreateTests {
     @AfterEach
     public void tearDown() {
         testHelper.tearDown();
+    }
+
+    @Test
+    @DisplayName("Should open add item modal on create button click.")
+    void ShouldOpenAddItemModalOnCreateButtonClick(){
+        testHelper.clickButtonById(addItemButtonId);
+        assertThat(testHelper.getWebElementById(addItemModalId).isDisplayed()).isTrue();
     }
 }
