4a2d69ad25b52bba0e1cce906b9b7b63404b5880
raphaelchiuso <raphael.chiuso@gmail.com>
2023-11-25 07:36:11 -0300

    test: should open delete items modal

--- coffeeshowtests/src/test/java/DeleteTests.java
+++ coffeeshowtests/src/test/java/DeleteTests.java
@@ -1,6 +1,30 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class DeleteTests {
     private final String deleteItemButtonId = "delete";
+    private final String deleteItemModalId = "chakra-modal-:R1qpf6:";
     private final TestHelper testHelper = new TestHelper();
+
+    @BeforeEach
+    public void setUp() {
+        testHelper.setUp();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        testHelper.tearDown();
+    }
+
+    @Test
+    @DisplayName("Should open delete items modal.")
+    void shouldOpenEditItemsModal(){
+        testHelper.clickButtonById(deleteItemButtonId);
+        assertThat(testHelper.getWebElementById(deleteItemModalId).isDisplayed()).isTrue();
+    }
 }
